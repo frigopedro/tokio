bf917baac16d5b9852e127ccc8510440dc4bfa43
Pedro Maia <pedromaia@Pedros-MacBook-Pro.local>
2022-10-04 13:46:29 -0300

    added email to contact

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -7,6 +7,9 @@ export default function Home() {
       <div>
         <p>seja bem vindo</p>
         <h1>Ainda estamos em construção</h1>
+        <a href="mailto:contato@tokioengenharia.com.br">
+          contato@tokioengenharia.com.br
+        </a>
       </div>
     </Container>
   );
--- frontend/src/styles/HomePageStyles.ts
+++ frontend/src/styles/HomePageStyles.ts
@@ -20,5 +20,9 @@ export const Container = styled.div`
     p {
       font-size: 1.5rem;
     }
+    a {
+      text-decoration: none;
+      color: #fc6b24;
+    }
   }
 `;
