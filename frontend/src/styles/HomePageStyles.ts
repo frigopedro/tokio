4a939d3e7d2c979424bbe22d60eec149c7b39edd
Pedro Maia <pedromaia@Pedros-MacBook-Pro.local>
2022-10-27 22:19:51 -0300

    feat: enhance mobile experience with third section bigger view

--- frontend/src/styles/HomePageStyles.ts
+++ frontend/src/styles/HomePageStyles.ts
@@ -150,8 +150,7 @@ export const Container = styled.div`
 `;
 
 export const SecondSection = styled.div`
-  margin-top: 300px;
-  margin-top: 20vw;
+  margin-top: clamp(20vh, 700px, 50vw);
 
   .services-list {
     padding: 1rem;
